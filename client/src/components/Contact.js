17c42ca67f289f2567056d4ad6024f0d831c1d05
Gohar Attiq <goharattiq@hotmail.com>
2021-06-08 19:03:21 +0500

    Contact me Updated

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,28 +4,24 @@ import Footer from './Footer'
 const Contact = () => {
   return (
     <Fragment>
-       <main id="contact">
-    <h1 className="lg-heading">
-      Contact
-      <span className="text-secondary">Me</span>
-    </h1>
-    <h2 className="sm-heading">
-      This is how you can reach me...
-    </h2>
-    <div className="boxes">
-      <div>
-        <span className="text-secondary">Email: </span> goharattiq@hotmail.com
-      </div>
-      <div>
-        <span className="text-secondary">Phone: </span> (+92) 304 4891824
-      </div>
-      {/* <div>
-        <span className="text-secondary">Address: </span> 50 Main st Boston MA 02101
-      </div> */}
-    </div>
-  </main>
-
-  <Footer/>
+      <main id="contact">
+        <h1 className="lg-heading">
+          Contact
+          <span className="text-secondary">Me</span>
+        </h1>
+        <h2 className="sm-heading">
+          This is how you can reach me...
+        </h2>
+        <div className="boxes">
+          <div>
+            <span className="text-secondary">Email: </span> goharattiq@hotmail.com
+          </div>
+          <div>
+            <span className="text-secondary">Phone: </span> (+92) 304 4891824
+          </div>
+        </div>
+      </main>
+      <Footer/>
     </Fragment>
   )
 }
