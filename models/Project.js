39382b399d7a7a73adb6d5ab5b1d61bbf2d433b1
Gohar Attiq <gohar.attiq@arbisoft.com>
2021-12-25 18:42:58 +0500

    Add DB_URL env variable

--- models/Project.js
+++ models/Project.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProjectSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   github: {
     type: String,
-    required: true
+    required: true,
   },
   images: [
     {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
   ],
   projectURL: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = Project = mongoose.model('projects', ProjectSchema);
+module.exports = Project = mongoose.model("projects", ProjectSchema);
--- routes/api/Work.js
+++ routes/api/Work.js
@@ -1,18 +1,17 @@
-const express = require('express'),
-      router = express.Router(),
-      Project    = require('../../models/Project');
+const express = require("express"),
+  router = express.Router(),
+  Project = require("../../models/Project");
 
-router.get('/',(req, res) =>{
-    Project.find()
-    .then(projects => {
+router.get("/", (req, res) => {
+  Project.find()
+    .then((projects) => {
       if (!projects) {
-        errors.noprofile = 'There are no profiles';
+        errors.noprofile = "There are no profiles";
         return res.status(404).json(errors);
       }
       res.json(projects);
     })
-    .catch(err => res.status(404).json({ msg: 'There are no profiles' }));
+    .catch((err) => res.status(404).json({ msg: "There are no profiles" }));
 });
-
 
 module.exports = router;
